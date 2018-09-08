a21e0ed8fa7a08640782191f2834b36852294aef
fenglinfang <nianbeiai@163.com>
2018-09-08 15:01:29 +0800

    登陆activity

---
+++ app/src/main/java/com/example/hjqm/testindex/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.hjqm.testindex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 这是登陆activity
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
